5adcce7a7bb406791b1bad94352964ccdf9c4291
Connor Wilson <Connorwilson1@googlemail.com>
2014-10-01 15:06:33 +0100

    Updated ElasticSearchOutput
    Changed request method and allowed elasticsearch to generate its own
    ID's

--- TreeBeard/TreeBeard.Plugins/Scripts/Outputs/ElasticSearchOutput.cs
+++ TreeBeard/TreeBeard.Plugins/Scripts/Outputs/ElasticSearchOutput.cs
@@ -5,6 +5,10 @@ using Microsoft.SqlServer.Server;
 using TreeBeard;
 using TreeBeard.Outputs;
 
+/// <summary>
+/// Writes event to elasticsearch instance.
+/// </summary>
+/// <arg name="uri" required="yes" example="http://localhost:9200">URI of elasticsearch instance</arg>
 public class ElasticSearchOutput : AbstractOutput
 {
     private string _uri;
@@ -12,22 +16,22 @@ public class ElasticSearchOutput : AbstractOutput
     public override void Execute(Event value)
     {
         string index = string.Format("treebeard-{0:yyyy-MM-dd}", value.EventTimeStamp);
-        _uri = string.Format("{0}/{1}/{2}/{3}", _uri, index, value.EventType, value.EventAlias);
+        _uri = string.Format("{0}/{1}/{2}", _uri, index, value.EventAlias);
         var httpWebRequest = (HttpWebRequest) WebRequest.Create(_uri);
         httpWebRequest.ContentType = "text/json";
-        httpWebRequest.Method = "PUT";
+        httpWebRequest.Method = "POST";
 
         using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
         {
            
-            streamWriter.Write(value.AsJson());
+            streamWriter.Write(value.AsJson(true));
             streamWriter.Flush();
             streamWriter.Close();
 
             var httpResponse = (HttpWebResponse) httpWebRequest.GetResponse();
             using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
             {
-                string result = streamReader.ReadToEnd();
+                streamReader.ReadToEnd();
             }
         }
     }
