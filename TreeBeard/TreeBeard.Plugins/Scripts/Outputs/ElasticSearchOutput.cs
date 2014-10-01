cffca94fbeec5aaa8a9f0957b67be8c1498087df
Connor Wilson <Connorwilson1@googlemail.com>
2014-10-01 14:49:57 +0100

    ElasticSearchOutput
    Added an ElasticSearch output which accepts a base URI as an arg, eg
    "http://localhost:9200" which may vary depending on the ElasticSearch
    setup.

--- TreeBeard/TreeBeard.Plugins/Scripts/Outputs/ElasticSearchOutput.cs
+++ TreeBeard/TreeBeard.Plugins/Scripts/Outputs/ElasticSearchOutput.cs
@@ -1,24 +1,39 @@
 ﻿using System;
+using System.IO;
 using System.Net;
+using Microsoft.SqlServer.Server;
 using TreeBeard;
 using TreeBeard.Outputs;
 
 public class ElasticSearchOutput : AbstractOutput
 {
-    private Uri _uri;
+    private string _uri;
+
     public override void Execute(Event value)
     {
-        var valueJson = value.AsJson();
+        string index = string.Format("treebeard-{0:yyyy-MM-dd}", value.EventTimeStamp);
+        _uri = string.Format("{0}/{1}/{2}/{3}", _uri, index, value.EventType, value.EventAlias);
+        var httpWebRequest = (HttpWebRequest) WebRequest.Create(_uri);
+        httpWebRequest.ContentType = "text/json";
+        httpWebRequest.Method = "PUT";
 
-        using (WebClient client = new WebClient())
+        using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
         {
-            client.UploadStringAsync(_uri, valueJson);
-        };
+           
+            streamWriter.Write(value.AsJson());
+            streamWriter.Flush();
+            streamWriter.Close();
+
+            var httpResponse = (HttpWebResponse) httpWebRequest.GetResponse();
+            using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+            {
+                string result = streamReader.ReadToEnd();
+            }
+        }
     }
 
     public override void Initialize(params string[] args)
     {
-        _uri = new UriBuilder(args[0]).Uri;
+        _uri = args[0];
     }
 }
-
